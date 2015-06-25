1e17a5f371fc5615ec282cbaf0e0589635a75a11
Zhuoqing Fang <fztsing@126.com>
2015-06-25 14:42:14 -0700

    Update readExcel.py

--- readExcel.py
+++ readExcel.py
@@ -2,53 +2,47 @@
 This script used for read excel output from ABI ViiA7 qPCR machine.
 Extract Data for further computing
 '''
+import argparse
 
-import pandas as pd
+
+# parse command line args
+parser = argparse.ArgumentParser(description="Extract qpcr data from ABi ViiA 7 machine")
+
+parser.add_argument("-f","--excelFile", action="store", dest="file",help="the excel file you want to analysis ")
+parser.add_argument("-s","--sheetName", action="store",default="Results", dest="sheet", \
+                     help="the sheet name of your excel file you want to analysis ")
+parser.add_argument("--header", action="store",type=int,dest="head", default=35,\
+                     help="header row you want to start with")
+parser.add_argument("--tail",action="store",type=int,dest="tail",default=5,\
+                     help="the tail rows of your excel file you want to drop")
+parser.add_argument("-r","--referenceControl", action="store", default="GAPDH", dest="rc", \
+                     help="the reference gene name of your sample, e.g. GAPDH")
+parser.add_argument("-c","--experimentalControl",action="store",dest="ec",\
+                     help="the control group name which your want to compare, e.g. hESC")
+parser.add_argument("-o","--outFileNamePrefix",action="store",default="foo",dest="out",\
+                     help="the output file name")
+parser.add_argument("--version",action="version",version="%(prog)s 1.0")
+args = parser.parse_args()
+
+print "ExeclFile        =", args.file
+print "SheetName        =", args.sheet
+print "headerRow        =", args.head
+print "tailRow          =", args.tail
+print "ReferenceControl =", args.rc
+print "ExperimentControl=", args.ec
+print "outFileName      =", args.out
+
 import numpy as np
-import os
-import getopt
-import sys
-####################################################################
-def	show_help():
-	print "USAGE:\tpython readExel.py <--input_file=input_file> [OPTIONS]"
-	print "OPTIONS"
-	print "-i or --input "
-	sys.exit()
+import pandas as pd
 
 
-def read_cmd_line ():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["input_file=", "ic=", "interctrl=", \
-                                                       "internal_control=", "ec=", "expCtrl=", \
-                                                       "experiment_control=", "help"])
-    except getopt.GetoptError:
-        print "illegal params!"
-        sys.exit()
-	global input_file, internal_control, experiment_control
-    for op, value in opts:
-		if op == '--input_file':
-			input_file=value
-		if op == '--ic' or op == '--internal_control' or re.search('^--interCtrl', op, re.I) :
-			internal_control=value
-		if op == '--ec' or op == '--experiment_control' or re.search('^--expCtrl', op, re.I):
-			experiment_control=value
-		elif op == '--help' or op =='-h':
-			show_help()
-##############################################################################
-
-
-
-# Params to specify 
-input_file="/path/to/your/.xls/file"
-output_file_name = "foo.xlsx"
-sheetName="sheetname"
-header=35  #header line begin
 columns = ['Sample Name','Target Name','CT','Ct Mean','Ct SD']
 
 # Read data into pandas DataFrame Object
-data0 = pd.read_excel(file,sheetname=sheetName,header=header)
-data = data0.drop(data0.tail(5).index) # remove last 5 row
+data0 = pd.read_excel(args.file,sheetname=args.sheet,header= args.head)
+data = data0.drop(data0.tail(tailRowDrop).index) # remove last 5 row
 dat = data[columns] # extract data wich we are interest
 
 # write to a new excel file
-dat.to_excel(output_file_name,'Results',index_col= None, na_values = ['NA'])
+dat.to_excel(args.out+"_interest_data.xls",'Results',index_col= None, na_values = ['NA'])
+data.to_excel(args.out+".xls",'Results',index_col= None, na_values = ['NA'])
